c06a9bb9ee600787e579bc02cca78d2bde8ce72b
João Henrique <joaoh3326@gmail.com>
2018-06-07 13:57:21 -0300

    Finalizado modelo de negociacao 	- Finalizado a aplicacao de imutabilidade 	- Utilizado Object.assign() para melhor funcionamento da 	atribuicao de propriedades na classe 	- Aterado var para let

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,9 +1,10 @@
 class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor) {
+        Object.assign(this, {_quantidade, _valor });
+        this._data = new Date(_data.getTime());
         Object.freeze(this);
+        // this._quantidade = quantidade;
+        // this._valor = valor;
     }
     
     get volume() {
