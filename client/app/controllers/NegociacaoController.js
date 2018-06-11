6f9b7259476d6c5b65f9841f18e876016102f742
João Henrique <joaoh3326@gmail.com>
2018-06-11 13:56:13 -0300

    Criando uma instancia de Negociacao 	- Esta sendo implementada a funÃÃo para criar uma instancia de 	negociacao 	- Resolvendo o problema de conversÃo de datas

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,8 +1,24 @@
 class NegociacaoController {
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
 
     adiciona(event) {
         event.preventDefault();
 
-        alert('Chamei ação no controller');
+        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        console.log(data)
+
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
     }
 }
