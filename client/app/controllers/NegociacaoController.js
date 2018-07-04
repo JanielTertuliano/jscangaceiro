b894e3ad90a1cfabb597bc5848d6ebf150c63e43
João Henrique <joaoh3326@gmail.com>
2018-07-04 14:01:38 -0300

    Aplicado mais um padrÃ£o de projeto factory 	- foi criado o diretorio util 	- foi adicionado o arquivo ProxyFactory 	- foi aplicado o factory para poder gerar o bind 	em negociacaoController

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,22 +7,28 @@ class NegociacaoController {
         this._inputValor = $('#valor');
         
         const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            get(target, prop, receiver) {
-                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia'].includes(prop)) {
+        this._negociacoes = ProxyFactory.create(
+            new Negociacoes(),
+            ['adiciona', 'esvazia'],
+            model => this._negociacoesView.update(model)
+        );
+        
+        // new Proxy(new Negociacoes(), {
+        //     get(target, prop, receiver) {
+        //         if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia'].includes(prop)) {
 
-                    return function() {
-                        console.log(`${prop} disparou a armadilha`);
-                        target[prop].apply(target, arguments);
+        //             return function() {
+        //                 console.log(`${prop} disparou a armadilha`);
+        //                 target[prop].apply(target, arguments);
 
-                        self._negociacoesView.update(target);
-                    }
+        //                 self._negociacoesView.update(target);
+        //             }
 
-                } else {
-                    return target[prop];
-                }
-            }
-        })
+        //         } else {
+        //             return target[prop];
+        //         }
+        //     }
+        // })
 
 
         this._negociacoesView = new NegociacoesView('#negociacoes');
